4ec7afa33822cb27bae441de3be6f354116ba39a
Sergio:P <dark07perez@gmail.com>
2018-02-08 00:25:25 -0500

    termianda parte 2

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,23 +1,50 @@
-import{Injectable}from'@angular/core';
+import { Injectable } from '@angular/core';
+import { User } from '../models/user';
 
-import { User }from '../models/user';
+import { APIService } from '../common/api.service';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
-export class UserService {private usuarios: User[] = [
-	new User('usuario 1','perez', ''),
-    new User('usuario 2','perez', ''),
-    new User('usuario 3','perez', '')
-  ];
+export class UserService extends APIService {
+	private resourceUrl = 'api/user';
+	private usuarios: User[] = [
+		new User('usuario 1', 'perez', ''),
+		new User('usuario 2', 'perez', ''),
+		new User('usuario 3', 'perez', '')
+	];
 
-  constructor() {
 
-  }
+	create(name: string, lastname: string, image: string): Observable<User> {
+		//this.todos.push(new Todo(description, priority, completed));
+		return this.post(this.resourceUrl, new User(name, lastname, image));
+	}
 
-  list(): User[] {
-    return this.usuarios;
-  }
+	list(): Observable<User[]> {
+		return this.get(this.resourceUrl);
+	}
 
-  create(name: string, lastname: string, image: string) {
-    this.usuarios.push(new User(name, lastname, image));
-  }
+
+	login(username: string, password: string) {
+		return this.post('user/login', { username, password }, { credentials: false }).map(loginResponse => {
+		  if (loginResponse) {
+			this.authService.accessToken = loginResponse.accessToken;
+		  }
+		});
+	  }
+
+	/*
+	constructor() {
+  
+	}*/
+
+
+	/*
+	list(): User[] {
+	  return this.usuarios;
+	}
+  
+	create(name: string, lastname: string, image: string) {
+	  this.usuarios.push(new User(name, lastname, image));
+	}
+	*/
 }
