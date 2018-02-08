e8cfd62a6db7cf671087f2b2c02c073e091c9318
Sergio:P <dark07perez@gmail.com>
2018-02-07 19:59:45 -0500

    avance, solucionando problemas

--- src/app/models/todo.ts
+++ src/app/models/todo.ts
@@ -1,11 +1,11 @@
 export class Todo {
-    private description:string;
+	private description: string;
 	private priority: Number;
-	private completed: boolean;exportclas
+	private completed: boolean;
 
-constructor(description: string, priority: Number = 1, completed: boolean = false) {
-        this.description = description;
-        this.completed = completed;
-        this.priority = priority;
-    }
+	constructor(description: string, priority: Number = 1, completed: boolean = false) {
+		this.description = description;
+		this.completed = completed;
+		this.priority = priority;
+	}
 }
