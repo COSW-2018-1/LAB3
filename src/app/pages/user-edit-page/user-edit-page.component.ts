8a87698ec109822b5587824e342671f6f377afa7
Sergio:P <dark07perez@gmail.com>
2018-02-08 00:10:55 -0500

    parte 2 terminada

---
+++ src/app/pages/user-edit-page/user-edit-page.component.ts
@@ -0,0 +1,41 @@
+import{Component, OnInit}from '@angular/core';
+import {Router}from '@angular/router';
+import {FormGroup, FormBuilder} from '@angular/forms';
+
+import {UserService}from '../../services/user.service';
+
+@Component({
+  selector: 'app-user-edit-page',
+  templateUrl: './user-edit-page.component.html',
+  styleUrls: ['./user-edit-page.component.css']
+})
+export class UserEditPageComponent implements OnInit {
+  private todoForm: FormGroup;
+
+  constructor(
+    public userService: UserService,
+    public formBuilder: FormBuilder,
+    public router: Router,
+  ) {
+
+  }
+
+  ngOnInit() {
+    this.todoForm = this.formBuilder.group({
+      name: '',
+      lastname: '',
+      image: ''
+    });
+  }
+
+  onSubmit() {
+    this.userService.create(
+      this.todoForm.get('name').value,
+      this.todoForm.get('lastname').value,
+      this.todoForm.get('image').value
+    );
+
+    this.router.navigate(['/user']);
+  }
+
+}
